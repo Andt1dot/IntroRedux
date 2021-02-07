221e6c5fd9ceea1a9be08ff6e8edb02e2991a6d3
vladmorari <morariv67@gmail.com>
2021-02-07 23:38:34 +0200

    meteo component done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,7 @@ const Home = () => {
     <div className="home-pge" >
       <ContentHome />
       <div>
-        Hello, you are in the country ... in the city. We know your IP address
-        and more about you we invite you to keep abreast of all meteo and
-        currency exchanges you decided to be one step ahead by going to this
-        site We are constantly growing, and if you want to help us, you have
-        already done so.
+     
       </div>
     </div>
   );
