68b9a8f58f5592fe04a0c99250860b72c52fd627
vladmorari <morariv67@gmail.com>
2021-02-10 00:57:42 +0200

    +

--- src/pages/Exchange.js
+++ src/pages/Exchange.js
@@ -1,10 +1,11 @@
-import ContentExchange from '../components/ContentExchange'
+import ContentExchange from "../components/ContentExchange";
 
-
-const Exchange = () =>{
-
-return (<ContentExchange/>);
-
-} 
+const Exchange = () => {
+  return (
+    <div className="bg-pge">
+      <ContentExchange />
+    </div>
+  );
+};
 
 export default Exchange;
